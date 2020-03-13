244762271e1addb706d6b86b1819cc8f9497e04e
Mark Wainwright <wainwrightml@googlemail.com>
2020-03-13 15:24:59 +0000

    Initial Commit

---
+++ EDR/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Reductech.EDR.Connectors.Nuix;
+using Reductech.EDR.Connectors.Nuix.Console;
+using Reductech.EDR.Connectors.Nuix.processes;
+using Reductech.EDR.Utilities.Processes;
+using Reductech.EDR.Utilities.Processes.enumerations;
+using Reductech.EDR.Utilities.Processes.injection;
+using Reductech.Utilities.InstantConsole;
+using Process = Reductech.EDR.Utilities.Processes.Process;
+
+namespace Reductech.EDR
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.OutputEncoding = Encoding.UTF8;
+
+            var useDongleString =  ConfigurationManager.AppSettings["NuixUseDongle"];
+            var nuixExeConsolePath = ConfigurationManager.AppSettings["NuixExeConsolePath"];
+
+            if (!bool.TryParse(useDongleString, out var useDongle))
+            {
+                System.Console.WriteLine("Please set the property 'NuixUseDongle' in the settings file");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(nuixExeConsolePath))
+            {
+                System.Console.WriteLine("Please set the property 'NuixExeConsolePath' in the settings file");
+                return;
+            }
+
+
+            var rubyScriptProcessAssembly = Assembly.GetAssembly(typeof(RubyScriptProcess));
+            Debug.Assert(rubyScriptProcessAssembly != null, nameof(rubyScriptProcessAssembly) + " != null");
+
+            var processAssembly = Assembly.GetAssembly(typeof(Process));
+            Debug.Assert(processAssembly != null, nameof(processAssembly) + " != null");
+
+            var nuixProcessSettings = new NuixProcessSettings(useDongle, nuixExeConsolePath);
+
+            var methods = typeof(YamlRunner).GetMethods()
+                .Where(m=>m.DeclaringType != typeof(object))
+                    .Select(x=>x.AsRunnable(new YamlRunner(nuixProcessSettings), new DocumentationCategory("Yaml")))
+                .OfType<IDocumented>()
+                .OrderBy(x=>x.Name)
+                .Concat(processAssembly.GetTypes()
+                    .Where(t=> 
+                        typeof(Process).IsAssignableFrom(t)
+                        
+                        )
+                    .Where(t=>!t.IsAbstract)
+                    .Select(x=> new YamlObjectWrapper(x, new DocumentationCategory("General Processes", typeof(Process)))
+
+                ).OrderBy(x=>x.Name))
+                .Concat(processAssembly.GetTypes()
+                    .Where(t=> 
+                        typeof(Enumeration).IsAssignableFrom(t) || typeof(Injection).IsAssignableFrom(t))
+                    .Where(t=>!t.IsAbstract)
+                    .Select(x=> new YamlObjectWrapper(x, new DocumentationCategory("Enumerations", typeof(Enumeration)))
+                    ).OrderBy(x=>x.Name))
+                
+
+                    .Concat(rubyScriptProcessAssembly.GetTypes()
+                        .Where(t=> typeof(RubyScriptProcess).IsAssignableFrom(t))
+                        .Where(t=>!t.IsAbstract)
+                        .Select(x=> new ProcessWrapper<INuixProcessSettings>(x, nuixProcessSettings, new DocumentationCategory("Nuix Processes"))
+                        ).OrderBy(x=>x.Name))
+
+                .ToList();
+
+            ConsoleView.Run(args, methods);
+        }
+    }
+}

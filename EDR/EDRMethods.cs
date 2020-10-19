bad99218dc99d691f997b8ed48f7dc94384e011b
Mark Wainwright <wainwrightml@gmail.com>
2020-10-19 15:27:05 +0100

    Moved in console methods from core

---
+++ EDR/EDRMethods.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CommandDotNet;
+using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging;
+using Reductech.EDR.Connectors.Nuix;
+using Reductech.EDR.Connectors.Nuix.Steps.Meta;
+using Reductech.EDR.Core;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Serialization;
+using Reductech.EDR.Core.Util;
+
+namespace Reductech.EDR
+{
+    [Command(Description = "Executes Nuix Sequences")]
+    internal class EDRMethods
+    {
+
+        [DefaultMethod]
+        [Command(Name = "execute", Description = "Execute a step defined in yaml")]
+        public Task Execute(
+            CancellationToken cancellationToken,
+            [Option(LongName = "command", ShortName = "c", Description = "The command to execute")]
+            string? yaml = null,
+            [Option(LongName = "path", ShortName = "p", Description = "The path to the yaml to execute")]
+            string? path = null) => ExecuteAbstractAsync(yaml, path, cancellationToken);
+
+
+        /// <summary>
+        /// Executes yaml
+        /// </summary>
+        private async Task ExecuteAbstractAsync(string? yaml, string? path, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(yaml))
+            {
+                if (string.IsNullOrWhiteSpace(path))
+                {
+                    throw new ArgumentException($"Please provide either {nameof(yaml)} or {nameof(path)}");
+                }
+
+                await ExecuteYamlFromPathAsync(path, cancellationToken);
+            }
+
+            else
+            {
+                if (string.IsNullOrWhiteSpace(path))
+                    await ExecuteYamlStringAsync(yaml, cancellationToken);
+                else
+                    throw new ArgumentException($"Please provide only one of {nameof(yaml)} or {nameof(path)}");
+            }
+        }
+
+
+
+        private async Task ExecuteYamlFromPathAsync(string path, CancellationToken cancellationToken)
+        {
+            var text = await File.ReadAllTextAsync(path, cancellationToken);
+
+            await ExecuteYamlStringAsync(text, cancellationToken);
+        }
+
+        /// <summary>
+        /// Runs a step defined in a yaml string
+        /// </summary>
+        private async Task ExecuteYamlStringAsync(string yaml, CancellationToken cancellationToken)
+        {
+            var stepFactoryStore =
+                StepFactoryStore.CreateUsingReflection(ConnectorTypes.Append(typeof(IStep)).ToArray());
+
+            var freezeResult = YamlMethods.DeserializeFromYaml(yaml, stepFactoryStore)
+                .Bind(x => x.TryFreeze())
+                .BindCast<IStep, IStep<Unit>>();
+
+            if (freezeResult.IsFailure)
+                Logger.LogError(freezeResult.Error);
+            else
+            {
+                var settingsResult = TryGetSettings();
+
+                if (settingsResult.IsFailure)
+                    Logger.LogError(settingsResult.Error);
+                else
+                {
+                    var stateMonad = new StateMonad(Logger, settingsResult.Value, ExternalProcessRunner.Instance,
+                        stepFactoryStore);
+
+                    var runResult = await freezeResult.Value.Run(stateMonad, cancellationToken);
+
+                    if (runResult.IsFailure)
+                        foreach (var runError in runResult.Error.AllErrors)
+                            Logger.LogError(runError.Message);
+                }
+            }
+        }
+
+
+        private Result<ISettings> TryGetSettings()
+        {
+            var settingsResult = NuixSettings
+                .TryCreate(sn => ConfigurationManager.AppSettings[sn])
+                .Map(x => x as ISettings);
+
+            return settingsResult;
+        }
+
+        /// <summary>
+        /// One type for each connector.
+        /// </summary>
+        private IEnumerable<Type> ConnectorTypes { get; } = new List<Type> {typeof(IRubyScriptStep)};
+
+
+        private ILogger Logger => Program.Logger!;
+    }
+}
--- EDR/Program.cs
+++ EDR/Program.cs
@@ -1,57 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Threading;
 using System.Threading.Tasks;
 using CommandDotNet;
-using CSharpFunctionalExtensions;
 using Microsoft.Extensions.Logging;
-using Reductech.EDR.Connectors.Nuix;
-using Reductech.EDR.Connectors.Nuix.Steps.Meta;
-using Reductech.EDR.Core;
 using Reductech.EDR.Core.Internal;
 using NLog.Extensions.Logging;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Reductech.EDR
 {
-    [Command(Description = "Executes Nuix Sequences")]
-    internal class EDRMethods : ConsoleMethods
-    {
-
-        [DefaultMethod]
-        [Command(Name = "execute", Description = "Execute a step defined in yaml")]
-        public Task Execute(
-            CancellationToken cancellationToken,
-            [Option(LongName = "command", ShortName = "c", Description = "The command to execute")]
-            string? yaml = null,
-            [Option(LongName = "path", ShortName = "p", Description = "The path to the yaml to execute")]
-            string? path = null) => ExecuteAbstractAsync(yaml, path, cancellationToken);
-
-        [Command(Name = "documentation", Description = "Generate Documentation in Markdown format")]
-        public void Documentation(
-            [Option(LongName = "path", ShortName = "p", Description = "The path to the documentation file to write")]
-            string path)
-            => GenerateDocumentationAbstract(path);
-
-        /// <inheritdoc />
-        protected override Result<ISettings> TryGetSettings()
-        {
-            var settingsResult = NuixSettings
-                .TryCreate(sn => ConfigurationManager.AppSettings[sn])
-                .Map(x => x as ISettings);
-
-            return settingsResult;
-        }
-
-        /// <inheritdoc />
-        protected override IEnumerable<Type> ConnectorTypes { get; } = new List<Type>() { typeof(IRubyScriptStep) };
-
-        /// <inheritdoc />
-        protected override ILogger Logger => Program.Logger!;
-    }
-
-
     internal class Program
     {
         public static ILogger? Logger;
@@ -67,10 +23,12 @@ namespace Reductech.EDR
 
                 await appRunner.RunAsync(args);
             }
+#pragma warning disable CA1031 // Do not catch general exception types
             catch (Exception e)
             {
                 Logger.LogError(e, e.Message);
             }
+#pragma warning restore CA1031 // Do not catch general exception types
 
             await Task.Delay(1);
         }

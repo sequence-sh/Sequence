548549bee74c787851d2eba8ae0126fc5b8b3bfb
antonyoni <antony@reduc.tech>
2020-11-09 15:11:58 +0000

    Refactor config init to use appsettings.json Not ready for production - needs further refactoring

--- EDR/Program.cs
+++ EDR/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using CommandDotNet;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+using Reductech.EDR.Connectors.Nuix;
+using Reductech.EDR.Connectors.Nuix.Steps.Meta;
 
 namespace Reductech.EDR
 {
@@ -10,9 +16,33 @@ namespace Reductech.EDR
     {
         public static async Task Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName))
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            NLog.LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+
             var provider = new NLogLoggerProvider();
             var logger = provider.CreateLogger("Console Logger");
             EDRMethods.StaticLogger = logger;
+
+            var nuixConfig = config.GetSection(nameof(NuixSettings));
+            var featureList = nuixConfig.GetSection("NuixFeatures").Get<string[]>();
+
+            var nuixFeatures = new HashSet<NuixFeature>();
+            foreach (var feature in featureList)
+                if (Enum.TryParse(typeof(NuixFeature), feature, true, out var nf) && nf is NuixFeature nuixFeature)
+                    nuixFeatures.Add(nuixFeature);
+
+            var nuixSettings = new NuixSettings(
+                nuixConfig.GetValue<bool>("NuixUseDongle"),
+                nuixConfig.GetValue<string>("NuixExeConsolePath"),
+                nuixConfig.GetValue<Version>("NuixVersion"),
+                nuixFeatures
+            );
+
+            EDRMethods.StaticSettings = nuixSettings;
 
             try
             {

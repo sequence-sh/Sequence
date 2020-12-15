e090e4cd570fac721861ab337342263ecb4aa21d
antonyoni <antony@reduc.tech>
2020-12-15 23:02:08 +0000

    Add test for execute from file Add test for when both yaml and path are empty

--- EDR.Tests/EDRMethodsTests.cs
+++ EDR.Tests/EDRMethodsTests.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
 using CommandDotNet;
 using CommandDotNet.IoC.MicrosoftDependencyInjection;
 using CommandDotNet.TestTools;
@@ -14,17 +17,22 @@ namespace EDR.Tests
     public class EDRMethodsTests
     {
 
-        private IServiceProvider getDefaultServiceProvider(ILogger<EDRMethods> logger)
+        private static IServiceProvider GetDefaultServiceProvider(ILogger<EDRMethods> logger) =>
+            GetDefaultServiceProvider(logger, null);
+        
+        private static IServiceProvider GetDefaultServiceProvider(ILogger<EDRMethods> logger,
+            IFileSystem fileSystem)
         {
             var config = Options.Create(new NuixConfig
             {
                 UseDongle = true,
                 ExeConsolePath = "Test Path",
                 Version = new Version(0, 0),
-                Features = Array.Empty<string>()
+                Features = new[] { "CASE_CREATION", "METADATA_IMPORT" }
             });
 
-            var edrm = new EDRMethods(logger, config);
+            var edrm = fileSystem == null ? new EDRMethods(logger, config) :
+                new EDRMethods(logger, config, fileSystem);
 
             var serviceProvider = new ServiceCollection().AddSingleton(edrm).BuildServiceProvider();
 
@@ -32,10 +40,10 @@ namespace EDR.Tests
         }
 
         [Fact]
-        public void TestSuccessfulFunction()
+        public void Execute_WhenCommandFunctionIsSuccess_LogsMessage()
         {
             var logger = new TestLogger<EDRMethods>();
-            var sp = getDefaultServiceProvider(logger);
+            var sp = GetDefaultServiceProvider(logger);
 
             var result = new AppRunner<EDRMethods>()
                 .UseMicrosoftDependencyInjection(sp)
@@ -47,13 +55,37 @@ namespace EDR.Tests
 
             logger.LoggedValues.Should().BeEquivalentTo(new object[] { "Hello World" });
         }
-
-
+        
         [Fact]
-        public void TestFailingFunction()
+        public void Execute_WhenPathFunctionIsSuccess_LogsMessage()
         {
             var logger = new TestLogger<EDRMethods>();
-            var sp = getDefaultServiceProvider(logger);
+
+            var filePath = @"C:\config.scl";
+            
+            var fs = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { filePath, new MockFileData("Print(Value = 'Hello World')") }
+            });
+            
+            var sp = GetDefaultServiceProvider(logger, fs);
+
+            var result = new AppRunner<EDRMethods>()
+                .UseMicrosoftDependencyInjection(sp)
+                .UseDefaultMiddleware()
+                .RunInMem($"-p {filePath}");
+
+            result.ExitCode.Should().Be(0);
+            result.Console.OutText().Should().Be("");
+
+            logger.LoggedValues.Should().BeEquivalentTo(new object[] { "Hello World" });
+        }
+        
+        [Fact]
+        public void Execute_WhenFunctionIsFailure_LogsErrorMessage()
+        {
+            var logger = new TestLogger<EDRMethods>();
+            var sp = GetDefaultServiceProvider(logger);
 
             var result = new AppRunner<EDRMethods>()
                 .UseMicrosoftDependencyInjection(sp)
@@ -65,6 +97,21 @@ namespace EDR.Tests
 
             logger.LoggedValues.Should().BeEquivalentTo(new object[] { "The step 'Pront' does not exist", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 29, Idx: 28", "{Error} - {Location}" });
         }
-        
+
+        [Fact]
+        public void Execute_WhenYamlAndPathAreNull_Throws()
+        {
+            var logger = new TestLogger<EDRMethods>();
+            var sp = GetDefaultServiceProvider(logger);
+
+            var result = Assert.Throws<ArgumentException>(() => new AppRunner<EDRMethods>()
+                .UseMicrosoftDependencyInjection(sp)
+                .UseDefaultMiddleware()
+                .RunInMem("-c \"\"")
+            );
+
+            Assert.Equal("Please provide either yaml or path", result.Message);
+        }
+
     }
 }

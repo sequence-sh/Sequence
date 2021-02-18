dab2ba74104e5c2b75680a7e90408c8ccd5e7c8f
Mark Wainwright <Mark@reduc.tech>
2021-02-18 17:59:39 +0000

    Added temporary fix for unhandled exception

--- EDR/EDRMethods.cs
+++ EDR/EDRMethods.cs
@@ -11,8 +11,11 @@ using Reductech.EDR.Connectors.Sql.Steps;
 using Reductech.EDR.Core;
 using Reductech.EDR.Core.Abstractions;
 using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
+using Reductech.EDR.Core.Internal.Logging;
 using Reductech.EDR.Core.Internal.Parser;
 using Reductech.EDR.Core.Internal.Serialization;
+using Reductech.EDR.Core.Util;
 
 namespace Reductech.EDR
 {
@@ -135,7 +138,7 @@ public class EDRMethods
         }
         else
         {
-            var runner = new SCLRunner(_settings, _logger, stepFactoryStore, _externalContext);
+            var runner = new SCLRunner2(_settings, _logger, stepFactoryStore, _externalContext);
 
             var r = await runner.RunSequenceFromTextAsync(scl, cancellationToken);
 
@@ -161,6 +164,82 @@ public class EDRMethods
     /// </summary>
     private IEnumerable<Type> ConnectorTypes { get; } =
         new List<Type> { typeof(IRubyScriptStep), typeof(SqlInsert) };
+}
+
+/// <summary>
+/// Runs processes from Text
+/// </summary>
+public sealed class SCLRunner2
+{
+    /// <summary>
+    /// Creates a new SCL Runner
+    /// </summary>
+    public SCLRunner2(
+        SCLSettings settings,
+        ILogger logger,
+        StepFactoryStore stepFactoryStore,
+        IExternalContext externalContext,
+        params KeyValuePair<string, object>[] loggingData)
+    {
+        _settings         = settings;
+        _logger           = logger;
+        _stepFactoryStore = stepFactoryStore;
+        _externalContext  = externalContext;
+
+        _loggingData = loggingData.ToDictionary(x => x.Key, x => x.Value);
+    }
+
+    private readonly SCLSettings _settings;
+    private readonly ILogger _logger;
+    private readonly StepFactoryStore _stepFactoryStore;
+    private readonly IExternalContext _externalContext;
+
+    private readonly IReadOnlyDictionary<string, object> _loggingData;
+
+    /// <summary>
+    /// Run step defined in an SCL string.
+    /// </summary>
+    /// <param name="text">SCL representing the step.</param>
+    /// <param name="cancellationToken">Cancellation ErrorLocation</param>
+    /// <returns></returns>
+    public async Task<Result<Unit, IError>> RunSequenceFromTextAsync(
+        string text,
+        CancellationToken cancellationToken)
+    {
+        var stepResult = SCLParsing.ParseSequence(text)
+            .Bind(x => x.TryFreeze(_stepFactoryStore))
+            .Map(SCLRunner.ConvertToUnitStep);
+
+        if (stepResult.IsFailure)
+            return stepResult.ConvertFailure<Unit>();
+
+        using var loggingScope = _logger.BeginScope(_loggingData);
+
+        var stateMonad = new StateMonad(
+            _logger,
+            _settings,
+            _stepFactoryStore,
+            _externalContext
+        );
+
+        _logger.LogSituation(LogSituation.SequenceStarted);
+
+        var connectorSettings = _settings.Entity.TryGetValue(SCLSettings.ConnectorsKey);
+
+        if (connectorSettings.HasValue)
+        {
+            _logger.LogSituation(
+                LogSituation.ConnectorSettings,
+                connectorSettings.Value.Serialize()
+            );
+        }
+
+        var runResult = await stepResult.Value.Run(stateMonad, cancellationToken);
+
+        _logger.LogSituation(LogSituation.SequenceCompleted);
+
+        return runResult;
+    }
 }
 
 }

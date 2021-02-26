9802f78a518048b9d892bacdcf8daed77e9f944d
Mark Wainwright <Mark@reduc.tech>
2021-02-26 15:09:22 +0000

    Removed some commented out code

--- EDR/EDRMethods.cs
+++ EDR/EDRMethods.cs
@@ -188,85 +188,9 @@ public class EDRMethods
         {
             typeof(IRubyScriptStep), typeof(SqlInsert)
 #if INCLUDE_PWSH
-  ,typeof(PwshRunScript)
+,typeof(PwshRunScript)
 #endif
         };
 }
-
-///// <summary>
-///// Runs processes from Text
-///// </summary>
-//public sealed class SCLRunner2
-//{
-//    /// <summary>
-//    /// Creates a new SCL Runner
-//    /// </summary>
-//    public SCLRunner2(
-//        SCLSettings settings,
-//        ILogger logger,
-//        StepFactoryStore stepFactoryStore,
-//        IExternalContext externalContext,
-//        params KeyValuePair<string, object>[] loggingData)
-//    {
-//        _settings         = settings;
-//        _logger           = logger;
-//        _stepFactoryStore = stepFactoryStore;
-//        _externalContext  = externalContext;
-
-//        _loggingData = loggingData.ToDictionary(x => x.Key, x => x.Value);
-//    }
-
-//    private readonly SCLSettings _settings;
-//    private readonly ILogger _logger;
-//    private readonly StepFactoryStore _stepFactoryStore;
-//    private readonly IExternalContext _externalContext;
-
-//    private readonly IReadOnlyDictionary<string, object> _loggingData;
-
-//    /// <summary>
-//    /// Run step defined in an SCL string.
-//    /// </summary>
-//    /// <param name="text">SCL representing the step.</param>
-//    /// <param name="cancellationToken">Cancellation ErrorLocation</param>
-//    /// <returns></returns>
-//    public async Task<Result<Unit, IError>> RunSequenceFromTextAsync(
-//        string text,
-//        CancellationToken cancellationToken)
-//    {
-//        var stepResult = SCLParsing.ParseSequence(text)
-//            .Bind(x => x.TryFreeze(_stepFactoryStore))
-//            .Map(SCLRunner.ConvertToUnitStep);
-
-//        if (stepResult.IsFailure)
-//            return stepResult.ConvertFailure<Unit>();
-
-//        using var loggingScope = _logger.BeginScope(_loggingData);
-
-//        var stateMonad = new StateMonad(
-//            _logger,
-//            _settings,
-//            _stepFactoryStore,
-//            _externalContext
-//        );
-
-//        _logger.LogSituation(LogSituation.SequenceStarted);
-
-//        var connectorSettings = _settings.Entity.TryGetValue(SCLSettings.ConnectorsKey);
-
-//        if (connectorSettings.HasValue)
-//        {
-//            _logger.LogSituation(
-//                LogSituation.ConnectorSettings,
-//                connectorSettings.Value.Serialize()
-//            );
-//        }
-
-//        var runResult = await stepResult.Value.Run(stateMonad, cancellationToken);
-
-//        _logger.LogSituation(LogSituation.SequenceCompleted);
-
-//        return runResult;
-//    }
-//}
 
 }

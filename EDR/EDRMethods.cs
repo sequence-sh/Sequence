46e73877b0f6a280e0292a9c8a7869e7e8c3a0fd
Mark Wainwright <Mark@reduc.tech>
2021-02-26 09:33:28 +0000

    Updated Core

--- EDR/EDRMethods.cs
+++ EDR/EDRMethods.cs
@@ -142,9 +142,13 @@ public class EDRMethods
         }
         else
         {
-            var runner = new SCLRunner2(_settings, _logger, stepFactoryStore, _externalContext);
+            var runner = new SCLRunner(_settings, _logger, stepFactoryStore, _externalContext);
 
-            var r = await runner.RunSequenceFromTextAsync(scl, cancellationToken);
+            var r = await runner.RunSequenceFromTextAsync(
+                scl,
+                new Dictionary<string, object>(),
+                cancellationToken
+            );
 
             if (r.IsFailure)
                 SCLRunner.LogError(_logger, r.Error);
@@ -167,89 +171,89 @@ public class EDRMethods
     /// One type for each connector.
     /// </summary>
     private IEnumerable<Type> ConnectorTypes { get; } =
-        new List<Type> {
-            typeof(IRubyScriptStep),
-            typeof(SqlInsert)
+        new List<Type>
+        {
+            typeof(IRubyScriptStep), typeof(SqlInsert)
 #if INCLUDE_PWSH
-            ,typeof(PwshRunScript)
+          ,typeof(PwshRunScript)
 #endif
         };
 }
 
-/// <summary>
-/// Runs processes from Text
-/// </summary>
-public sealed class SCLRunner2
-{
-    /// <summary>
-    /// Creates a new SCL Runner
-    /// </summary>
-    public SCLRunner2(
-        SCLSettings settings,
-        ILogger logger,
-        StepFactoryStore stepFactoryStore,
-        IExternalContext externalContext,
-        params KeyValuePair<string, object>[] loggingData)
-    {
-        _settings         = settings;
-        _logger           = logger;
-        _stepFactoryStore = stepFactoryStore;
-        _externalContext  = externalContext;
+///// <summary>
+///// Runs processes from Text
+///// </summary>
+//public sealed class SCLRunner2
+//{
+//    /// <summary>
+//    /// Creates a new SCL Runner
+//    /// </summary>
+//    public SCLRunner2(
+//        SCLSettings settings,
+//        ILogger logger,
+//        StepFactoryStore stepFactoryStore,
+//        IExternalContext externalContext,
+//        params KeyValuePair<string, object>[] loggingData)
+//    {
+//        _settings         = settings;
+//        _logger           = logger;
+//        _stepFactoryStore = stepFactoryStore;
+//        _externalContext  = externalContext;
 
-        _loggingData = loggingData.ToDictionary(x => x.Key, x => x.Value);
-    }
+//        _loggingData = loggingData.ToDictionary(x => x.Key, x => x.Value);
+//    }
 
-    private readonly SCLSettings _settings;
-    private readonly ILogger _logger;
-    private readonly StepFactoryStore _stepFactoryStore;
-    private readonly IExternalContext _externalContext;
+//    private readonly SCLSettings _settings;
+//    private readonly ILogger _logger;
+//    private readonly StepFactoryStore _stepFactoryStore;
+//    private readonly IExternalContext _externalContext;
 
-    private readonly IReadOnlyDictionary<string, object> _loggingData;
+//    private readonly IReadOnlyDictionary<string, object> _loggingData;
 
-    /// <summary>
-    /// Run step defined in an SCL string.
-    /// </summary>
-    /// <param name="text">SCL representing the step.</param>
-    /// <param name="cancellationToken">Cancellation ErrorLocation</param>
-    /// <returns></returns>
-    public async Task<Result<Unit, IError>> RunSequenceFromTextAsync(
-        string text,
-        CancellationToken cancellationToken)
-    {
-        var stepResult = SCLParsing.ParseSequence(text)
-            .Bind(x => x.TryFreeze(_stepFactoryStore))
-            .Map(SCLRunner.ConvertToUnitStep);
+//    /// <summary>
+//    /// Run step defined in an SCL string.
+//    /// </summary>
+//    /// <param name="text">SCL representing the step.</param>
+//    /// <param name="cancellationToken">Cancellation ErrorLocation</param>
+//    /// <returns></returns>
+//    public async Task<Result<Unit, IError>> RunSequenceFromTextAsync(
+//        string text,
+//        CancellationToken cancellationToken)
+//    {
+//        var stepResult = SCLParsing.ParseSequence(text)
+//            .Bind(x => x.TryFreeze(_stepFactoryStore))
+//            .Map(SCLRunner.ConvertToUnitStep);
 
-        if (stepResult.IsFailure)
-            return stepResult.ConvertFailure<Unit>();
+//        if (stepResult.IsFailure)
+//            return stepResult.ConvertFailure<Unit>();
 
-        using var loggingScope = _logger.BeginScope(_loggingData);
+//        using var loggingScope = _logger.BeginScope(_loggingData);
 
-        var stateMonad = new StateMonad(
-            _logger,
-            _settings,
-            _stepFactoryStore,
-            _externalContext
-        );
+//        var stateMonad = new StateMonad(
+//            _logger,
+//            _settings,
+//            _stepFactoryStore,
+//            _externalContext
+//        );
 
-        _logger.LogSituation(LogSituation.SequenceStarted);
+//        _logger.LogSituation(LogSituation.SequenceStarted);
 
-        var connectorSettings = _settings.Entity.TryGetValue(SCLSettings.ConnectorsKey);
+//        var connectorSettings = _settings.Entity.TryGetValue(SCLSettings.ConnectorsKey);
 
-        if (connectorSettings.HasValue)
-        {
-            _logger.LogSituation(
-                LogSituation.ConnectorSettings,
-                connectorSettings.Value.Serialize()
-            );
-        }
+//        if (connectorSettings.HasValue)
+//        {
+//            _logger.LogSituation(
+//                LogSituation.ConnectorSettings,
+//                connectorSettings.Value.Serialize()
+//            );
+//        }
 
-        var runResult = await stepResult.Value.Run(stateMonad, cancellationToken);
+//        var runResult = await stepResult.Value.Run(stateMonad, cancellationToken);
 
-        _logger.LogSituation(LogSituation.SequenceCompleted);
+//        _logger.LogSituation(LogSituation.SequenceCompleted);
 
-        return runResult;
-    }
-}
+//        return runResult;
+//    }
+//}
 
 }

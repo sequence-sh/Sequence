2817035fbcee45cc4e808bad8159807421b1e31f
Mark Wainwright <wainwrightml@gmail.com>
2021-04-26 17:17:27 +0100

    Use plugins instead of packages

--- EDR/EDRMethods.cs
+++ EDR/EDRMethods.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO.Abstractions;
 using System.Threading;
 using System.Threading.Tasks;
 using CommandDotNet;
 using CSharpFunctionalExtensions;
 using Microsoft.Extensions.Logging;
-using Reductech.EDR.Connectors.Nuix.Steps.Meta;
-using Reductech.EDR.Connectors.Pwsh;
-using Reductech.EDR.Connectors.Sql.Steps;
 using Reductech.EDR.Core;
 using Reductech.EDR.Core.Abstractions;
 using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
 using Reductech.EDR.Core.Internal.Parser;
 using Reductech.EDR.Core.Internal.Serialization;
+using TypeReference = Reductech.EDR.Core.Internal.TypeReference;
 
 namespace Reductech.EDR
 {
@@ -25,26 +24,33 @@ namespace Reductech.EDR
 public class EDRMethods
 {
     private readonly ILogger<EDRMethods> _logger;
+    private readonly IFileSystem _fileSystem;
 
     private readonly SCLSettings _settings;
-    private readonly IExternalContext _externalContext;
+
+    /// <summary>
+    /// External context without injected contexts
+    /// </summary>
+    private readonly IExternalContext _baseExternalContext;
+
+    private const int Success = 0;
+    private const int Failure = 1;
 
     /// <summary>
     /// Instantiate EDRMethods using the default IFileSystem provider.
     /// </summary>
     /// <param name="logger">The logger.</param>
     /// <param name="settings">Configuration for connectors.</param>
-    public EDRMethods(ILogger<EDRMethods> logger, SCLSettings settings)
+    /// <param name="fileSystem">The file system</param>
+    public EDRMethods(ILogger<EDRMethods> logger, SCLSettings settings, IFileSystem fileSystem)
         : this(
             logger,
             settings,
             new ExternalContext(
-                ExternalContext.Default.FileSystemHelper,
                 ExternalContext.Default.ExternalProcessRunner,
-                ExternalContext.Default.Console,
-                (Connectors.Sql.DbConnectionFactory.DbConnectionName,
-                 Connectors.Sql.DbConnectionFactory.Instance) //SQL database stuff
-            )
+                ExternalContext.Default.Console
+            ),
+            fileSystem
         ) { }
 
     /// <summary>
@@ -52,15 +58,18 @@ public class EDRMethods
     /// </summary>
     /// <param name="logger">The logger.</param>
     /// <param name="settings">Configuration</param>
-    /// <param name="externalContext">The external context. Can be mocked for testing.</param>
+    /// <param name="baseExternalContext">The external context. Can be mocked for testing.</param>
+    /// <param name="fileSystem">The file system</param>
     public EDRMethods(
         ILogger<EDRMethods> logger,
         SCLSettings settings,
-        IExternalContext externalContext)
+        IExternalContext baseExternalContext,
+        IFileSystem fileSystem)
     {
-        _logger          = logger;
-        _externalContext = externalContext;
-        _settings        = settings;
+        _logger              = logger;
+        _baseExternalContext = baseExternalContext;
+        _fileSystem          = fileSystem;
+        _settings            = settings;
     }
 
     /// <summary>
@@ -115,19 +124,41 @@ public class EDRMethods
         bool validate,
         CancellationToken cancellationToken)
     {
-        var text =
-            await _externalContext.FileSystemHelper.File.ReadAllTextAsync(path, cancellationToken);
+        var text = await _fileSystem.File.ReadAllTextAsync(path, cancellationToken);
 
-        var stepFactoryStore = StepFactoryStore.CreateUsingReflection(
-            ConnectorTypes.Append(typeof(IStep)).ToArray()
-        );
+        var stepFactoryStore = StepFactoryStore.TryCreateFromSettings(_settings, _logger);
+
+        if (stepFactoryStore.IsFailure)
+        {
+            throw new Exception(stepFactoryStore.Error.AsString);
+        }
 
         if (validate)
         {
-            return ValidateSCL(text, stepFactoryStore);
+            return ValidateSCL(text, stepFactoryStore.Value);
         }
 
-        var runner = new SCLRunner(_settings, _logger, stepFactoryStore, _externalContext);
+        var externalContext = GetInjectedExternalContext(
+            stepFactoryStore.Value,
+            _baseExternalContext
+        );
+
+        if (externalContext.IsFailure)
+        {
+            SCLRunner.LogError(
+                _logger,
+                externalContext.Error.WithLocation(ErrorLocation.EmptyLocation)
+            );
+
+            return Failure;
+        }
+
+        var runner = new SCLRunner(
+            _settings,
+            _logger,
+            stepFactoryStore.Value,
+            externalContext.Value
+        );
 
         var r = await runner.RunSequence(
             text,
@@ -139,10 +170,28 @@ public class EDRMethods
         );
 
         if (!r.IsFailure)
-            return 0;
+            return Success;
 
         SCLRunner.LogError(_logger, r.Error);
-        return 1;
+        return Failure;
+    }
+
+    private Result<IExternalContext, IErrorBuilder> GetInjectedExternalContext(
+        StepFactoryStore sfs,
+        IExternalContext baseExternalContext)
+    {
+        var injectedContextsResult = sfs.TryGetInjectedContexts(_settings);
+
+        if (injectedContextsResult.IsFailure)
+            return injectedContextsResult.ConvertFailure<IExternalContext>();
+
+        var externalContext = new ExternalContext(
+            baseExternalContext.ExternalProcessRunner,
+            baseExternalContext.Console,
+            injectedContextsResult.Value
+        );
+
+        return externalContext;
     }
 
     private async Task<int> ExecuteFromStringAsync(
@@ -150,20 +199,41 @@ public class EDRMethods
         bool validate,
         CancellationToken cancellationToken)
     {
-        var stepFactoryStore = StepFactoryStore.CreateUsingReflection(
-            ConnectorTypes.Append(typeof(IStep)).ToArray()
-        );
+        var stepFactoryStore = StepFactoryStore.TryCreateFromSettings(_settings, _logger);
 
-        if (validate)
+        if (stepFactoryStore.IsFailure)
         {
-            return ValidateSCL(scl, stepFactoryStore);
+            throw new Exception(stepFactoryStore.Error.AsString);
         }
 
-        var runner = new SCLRunner(_settings, _logger, stepFactoryStore, _externalContext);
+        if (validate)
+            return ValidateSCL(scl, stepFactoryStore.Value);
+
+        var externalContext = GetInjectedExternalContext(
+            stepFactoryStore.Value,
+            _baseExternalContext
+        );
+
+        if (externalContext.IsFailure)
+        {
+            SCLRunner.LogError(
+                _logger,
+                externalContext.Error.WithLocation(ErrorLocation.EmptyLocation)
+            );
+
+            return Failure;
+        }
+
+        var runner = new SCLRunner(
+            _settings,
+            _logger,
+            stepFactoryStore.Value,
+            externalContext.Value
+        );
 
         var r = await runner.RunSequenceFromTextAsync(
             scl,
-            new Dictionary<string, object>(),
+            new Dictionary<string, object>() { { SCLRunner.SequenceIdName, Guid.NewGuid() } },
             cancellationToken
         );
 
@@ -176,28 +246,19 @@ public class EDRMethods
 
     private int ValidateSCL(string scl, StepFactoryStore stepFactoryStore)
     {
-        var stepResult = SCLParsing.ParseSequence(scl)
+        var stepResult = SCLParsing.TryParseStep(scl)
             .Bind(x => x.TryFreeze(TypeReference.Any.Instance, stepFactoryStore))
             .Map(SCLRunner.ConvertToUnitStep);
 
         if (stepResult.IsFailure)
         {
             SCLRunner.LogError(_logger, stepResult.Error);
-            return 1;
+            return Failure; //indicate failure
         }
 
-        else
-        {
-            _logger.LogInformation("Build Successful");
-            return 0;
-        }
+        _logger.LogInformation("Build Successful");
+        return Success;
     }
-
-    /// <summary>
-    /// One type for each connector.
-    /// </summary>
-    private IEnumerable<Type> ConnectorTypes { get; } =
-        new List<Type> { typeof(IRubyScriptStep), typeof(SqlInsert), typeof(PwshRunScript) };
 }
 
 }

79162543151a68b9db11cc7eb0d614cd59f35bbd
antonyoni <antony@reduc.tech>
2022-03-20 16:12:03 +0000

    Remove EncodeParameterArgument method

--- Sequence.Tests/RunCommandTests.cs
+++ Sequence.Tests/RunCommandTests.cs
@@ -173,22 +173,6 @@ public class RunCommandTests
             );
     }
 
-    ///// <summary>
-    ///// Encodes an argument for passing into a program
-    ///// </summary>
-    ///// <param name="original">The value that should be received by the program</param>
-    ///// <returns>The value which needs to be passed to the program for the original value 
-    ///// to come through</returns>
-    //private static string EncodeParameterArgument(string original)
-    //{
-    //    if (string.IsNullOrEmpty(original))
-    //        return original;
-
-    //    string value = Regex.Replace(original, @"(\\*)" + "\"", @"$1\$0");
-    //    value = Regex.Replace(value, @"^(.*\s.*?)(\\*)$", "\"$1$2$2\"");
-    //    return value;
-    //}
-
     [Fact]
     public void RunPath_WithInjectedVariables_WhenSCLFunctionIsSuccess_ReturnsSuccess()
     {
